ee935ceaa3c2b43393452777ed34b9804a649ad4
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-01-22 21:59:10 +0530

    initial commit

---
+++ components/dialogs/dialog.js
@@ -0,0 +1,20 @@
+(function() {    
+    var dialog = document.getElementById('dialog-one');    
+    document.getElementById('show-dialog-one').onclick = function() {    
+        dialog.show();    
+    };    
+    document.getElementById('hide-dialog-one-btn').onclick = function() {    
+        dialog.close();    
+    };    
+
+    var dialog2 = document.getElementById('dialog-two');    
+    document.getElementById('show-dialog-two').onclick = function() {    
+        dialog2.show();    
+    };    
+    document.getElementById('hide-dialog-two-yes').onclick = function() {    
+        dialog2.close();    
+    };
+    document.getElementById('hide-dialog-two-no').onclick = function() {    
+        dialog2.close();    
+    };
+})()
